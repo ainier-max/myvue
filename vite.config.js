1ede80554b83b8378bc7887797b0d475d715322b
ainier-max <982810642@qq.com>
2024-05-21 17:46:26 +0800

    修改提交

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,24 @@
-import {defineConfig} from 'vite'
-import pluginVue from '@vitejs/plugin-vue'
-import path  from 'path'
+import { defineConfig } from "vite";
+import pluginVue from "@vitejs/plugin-vue";
+import path from "path";
 
 export default defineConfig({
-    plugins: [pluginVue()],
-    resolve: {
-        alias: {
-            '@':path.resolve(__dirname, './src/')
-        }
+  plugins: [pluginVue()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src/"),
     },
-    server:{
-        host:"127.0.0.1",
-        port:"4000",
-        hmr:true,
-        usePolling:true,
-        proxy: {
-            "/cbc": {
-                target: "http://10.11.0.87:8087",
-                changeOrigin: true,
-            },
-        },
-    }
-
-})
+  },
+  server: {
+    host: "127.0.0.1",
+    port: "4000",
+    hmr: true,
+    usePolling: true,
+    proxy: {
+      "/cbc": {
+        target: "http://127.0.0.1:8087",
+        changeOrigin: true,
+      },
+    },
+  },
+});
